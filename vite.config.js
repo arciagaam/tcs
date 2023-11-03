de9d3934d873f8751206cec13b9d88c1ce6bfb41
Miguel Arciaga <miguelarciagaa@gmail.com>
2023-11-03 15:42:07 +0800

    deleted ht access vite updarte

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css', 
                 'resources/js/app.js',
+                'resources/js/asyncButton.js',
                 'resources/js/bootstrap.js',
                 'resources/js/calendar.js',
                 'resources/js/ISOInput.js',
